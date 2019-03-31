fca6b3f2039bdc8c376843b768681a23aae1cab4
Alex <zyklonex@gmail.com>
2019-03-30 18:30:15 -0600

    image example

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,10 +3,10 @@ import style from './style';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
+		<h1>Home, Home on the range</h1>
 		<div><button type="button">Happy Banana!</button></div>
-		<div><img src={require('/home/alex/dev/banana/src/assets/Image/happybanana.jpg')}/></div>
-		<p>This is the Home component.</p>
+		<div><img src={require('./happybanana.jpg')}/></div>
+		<p>This is my first attempting at coding and programming. It is fun and a PAIN IN THE ASS! anything worth having is worth working hard for.</p>
 	</div>
 );
 
